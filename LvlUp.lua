d553ed276f89b1a9953459b5b9a30200be03c17c
Zentyron <schniki96@gmail.com>
2015-10-26 21:09:54 +0100

    Update and rename params.json to LvlUp.lua

---
+++ LvlUp.lua
@@ -0,0 +1,204 @@
+PrintChat("Auto lvl Up Loaded")
+
+TableSkill = {}
+
+TableSkill["Zac"] = {_W, _Q, _E, _E, _E, _R, _E, _W, _E, _W, _R, _W, _W, _Q, _Q, _R, _Q, _Q}
+TableSkill["Aatrox"] = {  _W,  _Q,  _W,  _E,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Ahri"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Akali"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Alistar"] = {  _Q,  _E,  _W,  _Q,  _E,  _R,  _Q,  _E,  _Q,  _E,  _R,  _Q,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Amumu"] = {  _W,  _E,  _E,  _Q,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Anivia"] = {  _Q,  _E,  _Q,  _E,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _Q,  _Q,  _Q,  _W,  _R,  _W,  _W }
+TableSkill["Annie"] = {  _W,  _Q,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Ashe"] = {  _W,  _E,  _W,  _Q,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Azir"] = {  _W,  _Q,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Blitzcrank"] = {  _Q,  _E,  _W,  _E,  _W,  _R,  _E,  _W,  _E,  _W,  _R,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Brand"] = {  _W,  _E,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Braum"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Caitlyn"] = {  _W,  _Q,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Cassiopeia"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Chogath"] = {  _Q,  _E,  _W,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Corki"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W }
+TableSkill["Darius"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E }
+TableSkill["Diana"] = {  _W,  _Q,  _W,  _E,  _Q,  _R,  _Q,  _Q,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["DrMundo"] = {  _W,  _Q,  _E,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Draven"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Elise"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Evelynn"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Ezreal"] = {  _Q,  _E,  _W,  _W,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["FiddleSticks"] = {  _E,  _W,  _W,  _Q,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Fiora"] = {  _W,  _Q,  _E,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Fizz"] = {  _E,  _Q,  _W,  _Q,  _W,  _R,  _Q,  _Q,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Galio"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _E,  _E,  _W,  _W,  _R,  _E,  _E }
+TableSkill["Gnar"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Gangplank"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Garen"] = {  _Q,  _W,  _E,  _E,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Gragas"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E }
+TableSkill["Graves"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _E,  _R,  _E,  _E,  _E,  _W,  _R,  _W,  _W }
+TableSkill["Hecarim"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Heimerdinger"] = {  _Q,  _W,  _W,  _Q,  _Q,  _R,  _E,  _W,  _W,  _W,  _R,  _Q,  _Q,  _E,  _E,  _R,  _Q,  _Q }
+TableSkill["Irelia"] = {  _E,  _Q,  _W,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _Q,  _Q,  _E,  _Q,  _R,  _E,  _Q }
+TableSkill["Janna"] = {  _E,  _Q,  _E,  _W,  _E,  _R,  _E,  _W,  _E,  _W,  _Q,  _W,  _W,  _Q,  _Q,  _Q,  _R,  _R }
+TableSkill["JarvanIV"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _W,  _Q,  _R,  _E,  _E,  _E,  _W,  _R,  _W,  _W }
+TableSkill["Jax"] = {  _E,  _W,  _Q,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _Q,  _E,  _Q,  _Q,  _R,  _E,  _Q }
+TableSkill["Jayce"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Jinx"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Kalista"] = {  _E,  _W,  _Q,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Karma"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _E,  _E,  _Q,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Karthus"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Kassadin"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Katarina"] = {  _Q,  _E,  _W,  _W,  _W,  _R,  _W,  _E,  _W,  _Q,  _R,  _Q,  _Q,  _Q,  _E,  _R,  _E,  _E }
+TableSkill["Kayle"] = {  _E,  _W,  _E,  _Q,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Kennen"] = {  _Q,  _E,  _W,  _W,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Khazix"] = {  _Q,  _E,  _Q,  _W , _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["KogMaw"] = {  _W,  _E,  _W,  _Q,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Leblanc"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E }
+TableSkill["LeeSin"] = {  _E,  _Q,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Leona"] = {  _Q,  _E,  _W,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Lissandra"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Lucian"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Lulu"] = {  _E,  _W,  _Q,  _E,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Lux"] = {  _E,  _Q,  _E,  _W,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Malphite"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W }
+TableSkill["Malzahar"] = {  _Q,  _E,  _E,  _W,  _E,  _R,  _Q,  _E,  _Q,  _E,  _R,  _W,  _Q,  _W,  _Q,  _R,  _W,  _W }
+TableSkill["Maokai"] = {  _E,  _Q,  _W,  _E,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["MasterYi"] = {  _E,  _Q,  _E,  _Q,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _W,  _W,  _R,  _W,  _W }
+TableSkill["MissFortune"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["MonkeyKing"] = {  _E,  _Q,  _W,  _Q,  _Q,  _R,  _E,  _Q,  _E,  _Q,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Mordekaiser"] = {  _E,  _Q,  _E,  _W,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Morgana"] = { _W,  _Q,  _W,  _E,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+                     -- "SupTreeMorgana"{ _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Nami"] = {  _Q,  _W,  _E,  _W,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Nasus"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E }
+TableSkill["Nautilus"] = {  _W,  _E,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Nidalee"] = {  _W,  _E,  _Q,  _E,  _Q,  _R,  _E,  _W,  _E,  _Q,  _R,  _E,  _Q,  _Q,  _W,  _R,  _W,  _W }
+TableSkill["Nocturne"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Nunu"] = {  _E,  _Q,  _E,  _W,  _Q,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Olaf"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Orianna"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Pantheon"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W }
+TableSkill["Poppy"] = {  _E,  _W,  _Q,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _W,  _W,  _E,  _E,  _E,  _E,  _R,  _R }
+TableSkill["Quinn"] = {  _E,  _Q,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Rammus"] = {  _Q,  _W,  _E,  _E,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Rek'Sai"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Renekton"] = {  _W,  _Q,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Rengar"] = {  _Q,  _E,  _W,  _Q,  _Q,  _R,  _W,  _Q,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Riven"] = {  _Q,  _W,  _E,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Rumble"] = {  _E,  _Q,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Ryze"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Sejuani"] = {  _W,  _Q,  _E,  _E,  _W,  _R,  _E,  _W,  _E,  _E,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q }
+TableSkill["Shaco"] = {  _W,  _E,  _Q,  _E,  _E,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _W,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Shen"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Shyvana"] = {  _W,  _Q,  _W,  _E,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q }
+TableSkill["Singed"] = {  _Q,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _E,  _W,  _E,  _W,  _R,  _W,  _E }
+TableSkill["Sion"] = {  _Q,  _E,  _E,  _W,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Sivir"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E }
+TableSkill["Skarner"] = {  _Q,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _E,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Sona"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Soraka"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _E,  _R,  _W,  _E,  _W,  _E,  _R,  _W,  _E }
+TableSkill["Swain"] = {  _W,  _E,  _E,  _Q,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Syndra"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Talon"] = {  _W,  _E,  _Q,  _W,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Taric"] = {  _E,  _W,  _Q,  _W,  _W,  _R,  _Q,  _W,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Teemo"] = {  _Q,  _E,  _W,  _E,  _Q,  _R,  _E,  _E,  _E,  _Q,  _R,  _W,  _W,  _Q,  _W,  _R,  _W,  _Q }
+TableSkill["Thresh"] = {  _Q,  _E,  _W,  _W,  _W,  _R,  _W,  _E,  _W,  _E,  _R,  _E,  _E,  _Q,  _Q,  _R,  _Q,  _Q }
+TableSkill["Tristana"] = {  _E,  _W,  _Q,  _E,  _E,  _R,  _Q,  _Q,  _Q,  _Q,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Trundle"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _E,  _R,  _W,  _E,  _W,  _E,  _R,  _W,  _E }
+TableSkill["Tryndamere"] = {  _E,  _Q,  _W,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["TwistedFate"] = {  _W,  _Q,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Twitch"] = {  _Q,  _E,  _E,  _W,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _Q,  _W,  _W }
+TableSkill["Udyr"] = {  _R,  _W,  _E,  _R,  _R,  _W,  _R,  _W,  _R,  _W,  _W,  _Q,  _E,  _E,  _E,  _E,  _Q,  _Q }
+TableSkill["Urgot"] = {  _E,  _Q,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _E,  _R,  _W,  _E,  _W,  _E,  _R,  _W,  _E }
+TableSkill["Varus"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Vayne"] = { _Q,  _W,  _E,  _W,  _W,  _R,  _W,  _Q,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Veigar"] = { _Q,  _E,  _Q,  _W,  _Q,  _R,  _W,  _W,  _W,  _W,  _R,  _E,  _Q,  _Q,  _E,  _R,  _E,  _E }
+TableSkill["Velkoz"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _W,  _W,  _W,  _W,  _R,  _E,  _Q,  _Q,  _E,  _R,  _E,  _E }
+TableSkill["Vi"] = {  _E,  _Q,  _W,  _E,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _Q,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Viktor"] = {  _E,  _Q,  _W,  _E,  _E,  _R,  _E,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W }
+TableSkill["Vladimir"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Volibear"] = {  _W,  _E,  _W,  _Q,  _W,  _R,  _E,  _W,  _Q,  _W,  _R,  _E,  _Q,  _E,  _Q,  _R,  _E,  _Q }
+TableSkill["Warwick"] = {  _W,  _Q,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _E,  _W,  _R,  _W,  _W }
+TableSkill["Xerath"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["XinZhao"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Yorick"] = {  _W,  _E,  _Q,  _E,  _E,  _R,  _E,  _W,  _E,  _Q,  _R,  _W,  _Q,  _W,  _Q,  _R,  _W,  _Q }
+
+TableSkill["Zed"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Ziggs"] = {  _Q,  _W,  _E,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Zilean"] = {  _Q,  _W,  _Q,  _E,  _Q,  _R,  _Q,  _W,  _Q,  _W,  _R,  _W,  _W,  _E,  _E,  _R,  _E,  _E }
+TableSkill["Zyra"] = {  _E,  _W,  _Q,  _Q,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Yasuo"] = {  _Q,  _E,  _Q,  _W,  _Q,  _R,  _Q,  _E,  _Q,  _E,  _R,  _E,  _E,  _W,  _W,  _R,  _W,  _W }
+TableSkill["Bard"] = { _W,  _Q,  _E,  _W,  _W,  _R,  _Q,  _W,  _W,  _Q,  _R,  _Q,  _Q,  _E,  _E,  _R,  _E,  _E }
+
+function switch(c)
+  local swtbl = {
+    casevar = c,
+    caseof = function (self, code)
+      local f
+      if (self.casevar) then
+        f = code[self.casevar] or code.default
+      else
+        f = code.missing or code.default
+      end
+      if f then
+        if type(f)=="function" then
+          return f(self.casevar,self)
+        else
+          error("case "..tostring(self.casevar).." not a function")
+        end
+      end
+    end
+  }
+  return swtbl
+end
+
+function lvlup(x)
+	LevelSpell(TableSkill[GetObjectName(myHero)][x])
+end
+
+		AutoLvlUpConfig = scriptConfig("AutoLvlUp", "LvlUp.lua")
+		AutoLvlUpConfig.addParam("lvlauto", "Auto Lvl Up Skill", SCRIPT_PARAM_ONOFF, false)
+		AutoLvlUpConfig.addParam("lvlautoulti", "Auto Lvl Up Ulti", SCRIPT_PARAM_ONOFF, true)
+
+
+OnLoop(function(myHero)
+
+
+if TableSkill[GetObjectName(myHero)] == nil and AutoLvlUpConfig.lvlautoulti or not AutoLvlUpConfig.lvlauto then
+	if GetLevel(myHero) == 6 or GetLevel(myHero) == 11 or GetLevel(myHero) == 16 then
+		LevelSpell(_R)
+	end
+else
+
+    
+	 if AutoLvlUpConfig.lvlauto then
+	
+		switch(GetLevel(myHero)):caseof{
+			[1]   = function (x) lvlup(x) end,
+			[2]   = function (x) lvlup(x) end,
+			[3]   = function (x) lvlup(x) end,
+			[4]   = function (x) lvlup(x) end,
+			[5]   = function (x) lvlup(x) end,
+			[6]   = function (x) lvlup(x) end,
+			[7]   = function (x) lvlup(x) end,
+			[8]   = function (x) lvlup(x) end,
+			[9]   = function (x) lvlup(x) end,
+			[10]   = function (x) lvlup(x) end,
+			[11]   = function (x) lvlup(x) end,
+			[12]   = function (x) lvlup(x) end,
+			[13]   = function (x) lvlup(x) end,
+			[14]   = function (x) lvlup(x) end,
+			[15]   = function (x) lvlup(x) end,
+			[16]   = function (x) lvlup(x) end,
+			[17]   = function (x) lvlup(x) end,
+			[18]   = function (x) lvlup(x) end,
+			default = function (x) PrintChat("Lvl Number Error : " .. x)  end,
+			missing = function (x) PrintChat("error") end,
+		}
+	
+	 end
+	
+end
+
+	
+
+end)
